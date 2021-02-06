9b5acc67fa228f9479083938b483f0bf07c83953
kos-vkg <kvertenov@mail.ru>
2021-02-07 01:30:04 +0300

    junit4

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,9 +1,6 @@
 package ru.netology;
 
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
-import static org.testng.Assert.assertEquals;
+import org.junit.Test;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
@@ -11,18 +8,18 @@ public class CashbackHackServiceTest {
     @Test
     public void shouldtestNormal() {
         int actual = service.remain(900);
-        assertEquals(actual, 100);
+        org.junit.Assert.assertEquals(100,actual);
     }
 
     @Test
     public void shouldtestEmpty() {
         int actual = service.remain(0);
-        assertEquals(actual, 1000);
+        org.junit.Assert.assertEquals(1000,actual);
     }
 
     @Test
     public void shouldtestOptimal() {
         int actual = service.remain(1000);
-        assertEquals(actual, 0);
+        org.junit.Assert.assertEquals(0,actual);
     }
 }
